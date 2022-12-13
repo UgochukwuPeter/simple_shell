25a59ebd4284c262eba973f215f3ecdb45800060
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-13 16:04:29 +0100

    fix # issues

---
+++ conv_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * conv_line - converts a line to a command able to be run through execve
+ * @cmd: command entered
+ * @line: argument entered for cmd
+ *
+ * Return: a array of strings to be passed to execve
+ */
+char **conv_line(char *cmd, char *line)
+{
+	char **av;
+	int i, spac_num;
+
+	for (i = 0, spac_num = 0; line[i] != '#' && line[i] != '\0'; i++)
+	{
+		if (line[i] == ' ')
+			spac_num++;
+	}
+	av = malloc((spac_num + 2) * sizeof(void *));
+	if (line[i] == '#')
+		line[i] = '\0';
+	strtok(line, " ");
+	av[0] = cmd;
+	for (i = 1; i <= spac_num; i++)
+	{
+		av[i] = strtok(NULL, " ");
+	}
+	av[i] = NULL;
+	return (av);
+}
