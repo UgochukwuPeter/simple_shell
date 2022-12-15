d5dc3d8b210c0372068e12aca74463b6ab3ec755
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-15 18:59:24 +0100

    add ls current directory #cisfun features

---
+++ path_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * path_list - creates a linked list of the path
+ * @head: linked list head
+ *
+ * Return: returns the linked list head.
+ */
+list_t *path_list(list_t **head)
+{
+	char *name = "PATH";
+	char *value, *value1, *token;
+
+	value = _getenv(name);
+	value1 = value;
+	if (value == NULL)
+		return (NULL);
+	if (value[0] == ':')
+		add_node(head, ":");
+	token = strtok(value, ":");
+	while (token != NULL)
+	{
+		add_node_end(head, token);
+		token = strtok(NULL, ":");
+	}
+	free(value1);
+	return ((*head));
+}
