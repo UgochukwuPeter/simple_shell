f8c43af29b4e65c709f3f08ec03448c09db1aea1
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-15 04:07:38 +0100

    add help features

---
+++ getenv.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _getenv - get enviroment variables
+ * @env: env var to get
+ * Return: current envirment variables, of specified type or null if fail
+ */
+char *_getenv(char *env)
+{
+	int i, count, j;
+	char *pathcheck, *path, *value;
+	char **ev;
+
+	for (i = 0, count = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '=' && environ[i][j]; j++)
+			;
+		if (environ[i][j] == '=')
+			count++;
+	}
+	ev = malloc(++count * sizeof(void *));
+	for (i = 0; environ[i]; i++)
+	{
+		ev[i] = _strdup(environ[i]);
+		pathcheck = strtok(ev[i], "=");
+		if (!_strcmp(pathcheck, env))
+		{
+			path = strtok(NULL, "=");
+			value = _strdup(path);
+			for (j = 0; j <= i; j++)
+				free(ev[j]);
+			free(ev);
+			return (value);
+		}
+	}
+	for (j = 0; j < i; j++)
+		free(ev[j]);
+	free(ev);
+	return (NULL);
+}
+
