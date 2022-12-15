f7d25b83b9ad0c8f2edf5d7315c29aebaf5766df
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-15 03:55:31 +0100

    exit status

---
+++ exit_fn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * exit_fn - a function exit the application
+ * @h: a argument
+ * @d: currently an unused atribute
+ * Return: exit with 0 on success
+ */
+char *exit_fn(char *d, list_t *h)
+{
+	int n, status;
+
+	status = WEXITSTATUS(status);
+	n = (d == NULL) ? (status) : (_atoi(d));
+	free_list(h);
+	exit(n);
+}
