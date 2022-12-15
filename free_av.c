ab4b4b907a7ec0100e68be2b106c15c15a667166
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-15 03:57:35 +0100

    fixed some free memory

---
+++ free_av.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * free_av - a function that free the vector have been allocated memories
+ * @av: a vector array
+ * Return: void
+ */
+void free_av(char **av)
+{
+	int i = 0;
+
+	while (av != NULL && av[i] != NULL)
+	{
+		assert(av[i] != NULL);
+		free(av[i]);
+		++i;
+	}
+	assert(av != NULL);
+	if (av != NULL)
+		free(av);
+}
