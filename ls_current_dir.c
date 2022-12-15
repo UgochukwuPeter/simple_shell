41581ca10672dca70d4b0db5644befab8957db4b
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-15 18:55:49 +0100

    betty style, and Readme update.

---
+++ ls_current_dir.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * ls_current_dir - a function displays current working directory
+ * Return: returns the current working path
+ */
+char *ls_current_dir(void)
+{
+	char *value;
+
+	value = _getenv("PWD");
+	return (value);
+}
