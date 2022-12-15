d7bd3ebc1a9a09d3de75826c53f51d65c5e3c79e
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-15 04:05:08 +0100

    add exit build in fuction

---
+++ getcwd.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _getcwd - gets the current working dir
+ * Return: the current working dir
+ */
+char *_getcwd(void)
+{
+	char *dir = malloc(1024); /*max path size*/
+	size_t dirsize = 1024;
+
+	if (dir == NULL)
+	{
+		return (NULL);
+	}
+	getcwd(dir, dirsize);
+	return (dir);
+}
