8e2d52e47141bc8805d7359887ca3454115987a0
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-12-16 11:08:47 +0100

    setenv

---
+++ unsetenv_fn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * unsetenv_fn - a function unset enviroment variable
+ * @d: a string contains enviroment variable
+ * @h: an unused variable
+ * Return: returns NULL
+ */
+char *unsetenv_fn(char *d, __attribute__((unused)) list_t *h)
+{
+	int ret;
+
+	ret = unsetenv(d);
+	if (ret == -1)
+		perror("access denied");
+	return (NULL);
+}
